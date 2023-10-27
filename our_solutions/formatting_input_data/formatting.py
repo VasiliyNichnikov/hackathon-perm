bd785300e4319b8be0d634eddca2a4f072a08043
Filisss_911 <102216221+FilippYV@users.noreply.github.com>
2023-10-27 19:35:40 +0300

    Исправление ошибок в тансформации данных

--- our_solutions/formatting_input_data/formatting.py
+++ our_solutions/formatting_input_data/formatting.py
@@ -1,13 +1,11 @@
 import os
 
-from our_solutions.paths import get_no_sorted_data_path
+from our_solutions.paths import get_no_sorted_data_path, get_path_to_folder_for_transformed_data
 
-array_name = ['video0', 'video1', 'video1']
+array_name = ['video0', 'video1', 'video2']
 for name_txt in array_name:
     with open(get_no_sorted_data_path(f'{name_txt}'), 'r') as file:
         lines = file.readlines()
-
-    output_folder = f'../transformed_data/{name_txt}/'
 
     data_dict = {}
     for line in lines:
@@ -17,11 +15,13 @@ for name_txt in array_name:
             data_dict[photo_num] = []
         data_dict[photo_num].append(elements[2:])
 
-    # Запись данных в отдельные файлы в указанной папке
     for photo_num, data in data_dict.items():
         file_name = f'{photo_num}.txt'
-        file_name = file_name.replace(",", "")  # Удаление запятой из имени файла
-        file_path = os.path.join(output_folder, file_name)
+        file_name = file_name.replace(",", "")
+        folder_path = get_path_to_folder_for_transformed_data(name_txt)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        file_path = os.path.join(folder_path, file_name)
         with open(file_path, 'w') as file:
             for item in data:
                 file.write(' '.join(item) + '\n')
--- our_solutions/paths.py
+++ our_solutions/paths.py
@@ -3,8 +3,12 @@ from pathlib import Path
 
 
 def get_project_root():
-    return Path(__file__).parent.parent.parent
+    return Path(__file__).parent
 
 
 def get_no_sorted_data_path(name):
-    return os.path.join(get_project_root(), f'static/{name}.txt')
+    return os.path.join(get_project_root(), f'static/raw_data/{name}.txt')
+
+
+def get_path_to_folder_for_transformed_data(name):
+    return os.path.join(get_project_root(), f'static/transformed_data/{name}')
