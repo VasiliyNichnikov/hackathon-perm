0e5427fb665ae0a7c5eef538f081ea6307fbede9
v.nichnikov <57954863+VasiliyNichnikov@users.noreply.github.com>
2023-10-29 02:48:06 +0300

    Изменена верстка

--- app/analytics/controllers.py
+++ app/analytics/controllers.py
@@ -7,14 +7,11 @@ module = Blueprint("analytics", __name__)
 
 @module.route("/display/<filename>")
 def analytics_show_media(filename: str) -> str:
-    try:
-        output = ControllerByDataOutput()
-        view_data = output.get_view_data(filename)
-    except:
-        print("error")
+    output = ControllerByDataOutput()
+    view_data = output.get_view_data(filename)
 
     if is_image(filename):
-        return render_template("analytics.html", image_file=view_data.box_path, image_chart_file=view_data.chart_path, messages=view_data.messages)
+        return render_template("analytics2.html", image_file=view_data.box_path, image_chart_file=view_data.chart_path, messages=view_data.messages)
     elif is_video(filename):
         return render_template("analytics.html", video_file="upload/box/" + filename)
     abort(400)
--- app/entity/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileAllowed
-from wtforms import FileField
-
-# from app import allowed_files
-#
-#
-# class MediaForm(FlaskForm):
-#     file = FileField(validators=[FileAllowed(allowed_files, "Image and videos!")])
